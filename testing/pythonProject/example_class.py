a7bfb053d88ce6e0c40cc8e7ee1caa23c9fe8844
Elizabeth Reid (emr237) <elizabeth.reid@usask.ca>
2020-10-26 20:00:35 -0600

    Added an example User class and testing script for that class

---
+++ testing/pythonProject/example_class.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(self, type):
+        self.type = type
+
+    def hasSpotifyAccount(self):
+        # Doesn't matter what it does for now
+        return False
+
+    def isGuest(self):
+        # Doesn't matter what it does for now
+        return False
