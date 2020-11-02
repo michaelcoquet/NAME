b362a7c3890f54389064c61c094b00b46d7b3e25
m <m.coquet2@gmail.com>
2020-11-02 12:33:24 -0600

    adds a unit test for hasSpotifyAccount(), adds a pytest.ini to allow the -s option (may only be usefull for vscode, not sure), changes the member class to a user class since they seem very redundant and will jsut complicate things further

--- testing/proof_of_concept/__init__.py
+++ testing/proof_of_concept/__init__.py
@@ -1,3 +1,3 @@
-from .member_class import Member
+from .member_class import User
 from .playlist_class import Playlist
 from .song_class import Song
--- testing/unit_test.py
+++ testing/unit_test.py
@@ -1,19 +1,19 @@
 import os
+import pytest
 from proof_of_concept import Playlist
 from proof_of_concept import Song
-from proof_of_concept import Member
+from proof_of_concept import User
+
+# hardcoding for testing purposes
+client_id = "0f34fcaccbd64596a108401d6b020f1e" # this is the spotifyAPI developer clientID for developers only
+client_secret = "b2d9c4f194224634bde7b616eb45c04a"
+redirect_uri = "https://example.com/callback"
+username = "vha6pttyppu7tnrc0l1j4k4de"
+# this is a new spotify account created just for testing
+me = User()
 
 def test_getSpotifyPlaylists():
-
-    client_id = "0f34fcaccbd64596a108401d6b020f1e" # this is the spotifyAPI developer clientID for developers only
-    client_secret = "b2d9c4f194224634bde7b616eb45c04a"
-    redirect_uri = "https://example.com/callback"
-
-
-    # this is a new spotify account created just for testing
-    username = "vha6pttyppu7tnrc0l1j4k4de"
-
-    me = Member(username=username, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+    me.linkSpotifyAccount(username=username, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
 
     # print out each of the users saved playlists
     playlists = me.getSpotifyPlaylists()
@@ -23,6 +23,24 @@ def test_getSpotifyPlaylists():
     assert playlists['items'][1]['name'] == "testing1"
     assert playlists['items'][2]['name'] == "testing2"
 
+def test_hasSpotifyAccount():
+    # should still have a linked spotify account if the previous test passed
+    # so just a simple assert is required
+    assert me.isGuest == False
+
+    # clear the cache now (effectively forgetting the linked account)
+    clear_cache()
+
+    # now try giving a username with a missing character 
+    # the isGuest should now be True
+    me.linkSpotifyAccount(username="ha6pttyppu7tnrc0l1j4k4de", client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+    assert me.isGuest == True
+
+    # finally clear the cache again so it doesnt cause problems
+    # if the tests are ran again
+    clear_cache()
+    
+def clear_cache():
     # delete the caches (probably not the proper or ideal way to do this but good enough for testing)
     if os.path.exists(".cache"):
         os.remove(".cache")
