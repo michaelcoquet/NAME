8f875ca63e4b9584dbafed59dbc494d80ffc880a
m <m.coquet2@gmail.com>
2020-11-13 21:16:42 -0600

    finishs the basic song similarity search progress popup window

--- gui/home_page.py
+++ gui/home_page.py
@@ -1,8 +1,11 @@
 """The home screen for the app
 """
+import time
 import tkinter as tk
+from tkinter import ttk
 from tkinter import StringVar
 from tkinter import Grid
+
 
 class HomePage(tk.Frame):
     """ Could possibly be a splash screen but for now this is the home page screen
@@ -10,12 +13,18 @@ class HomePage(tk.Frame):
     Args:
         tk (Frame): main home page screen (no spotify account linked)
     """
+    max_songs = "6"
 
     # pylint: disable=too-many-instance-attributes
     # theres going to be lots of instance attributes for this class
+
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
+        self.win = None
+        self.home_frame = None
+        self.progress = None
+        self.l_songs_found = None
         self.create_widgets()
 
     def create_widgets(self):
@@ -23,17 +32,17 @@ class HomePage(tk.Frame):
         """
 
         #make home frame
-        home_frame = tk.Frame(self.master)
-        home_frame.grid(row=0, column=0, sticky="nsew")
+        self.home_frame = tk.Frame(self.master)
+        self.home_frame.grid(row=0, column=0, sticky="nsew")
 
-        upper_menu = tk.Frame(home_frame)
+        upper_menu = tk.Frame(self.home_frame)
         upper_menu.grid(row=0, column=0, sticky="ew")
-        lower_menu = tk.Frame(home_frame)
+        lower_menu = tk.Frame(self.home_frame)
         lower_menu.grid(row=2, column=0, sticky="ew")
 
         #set up grid
-        Grid.columnconfigure(home_frame, 0, weight=1)
-        Grid.rowconfigure(home_frame, 1, weight=1)
+        Grid.columnconfigure(self.home_frame, 0, weight=1)
+        Grid.rowconfigure(self.home_frame, 1, weight=1)
 
         self.grid(padx="10", pady="10")
 
@@ -70,13 +79,99 @@ class HomePage(tk.Frame):
         self.song_search_button["text"] = "Search"
         self.song_search_button.grid(row=2, column=2)
 
-        self.song_listbox = tk.Listbox(home_frame)
+        self.song_listbox = tk.Listbox(self.home_frame)
         self.song_listbox.grid(row=1, column=0, sticky="nsew")
 
         self.remove_all = tk.Button(lower_menu)
         self.remove_all["text"] = "Remove all"
         self.remove_all.pack(side=tk.LEFT)
 
-        self.create_similarity_playlist = tk.Button(lower_menu)
-        self.create_similarity_playlist["text"] = "Create Similarity Playlist"
+        self.create_similarity_playlist = tk.Button(lower_menu,
+            text="Create Similarity Playlist", command=self.open_sim_progress)
         self.create_similarity_playlist.pack(side=tk.RIGHT)
+
+    # function to open a new window
+    # on a button click
+    def open_sim_progress(self):
+        """open a new window that updates the user on the progress of similarity playlist
+           creation
+        """
+        self.grab_set()
+        # Toplevel object which will
+        # be treated as a new window
+        self.win = tk.Toplevel(self)
+        self.win.protocol("WM_DELETE_WINDOW", self.close_window)
+        # sets the title of the
+        # Toplevel widget
+        self.win.title("Searching")
+
+        l_1 = tk.Label(self.win, text="Finding Similar Songs!")
+        l_1.pack(side=tk.TOP)
+
+        self.l_songs_found = tk.Label(self.win, text="Songs found...   0/" + self.max_songs)
+        self.l_songs_found.pack(side=tk.TOP)
+
+        self.progress = tk.ttk.Progressbar(self.win, orient=tk.HORIZONTAL, length=200,
+            mode="determinate")
+
+        self.progress.pack(pady=10)
+
+        cancel_btn = tk.Button(self.win, text="Cancel", command=self.close_window)
+        cancel_btn.pack(side=tk.BOTTOM)
+
+        self.search_update()
+
+    def search_update(self):
+        """TODO: connect with song similarity search
+            This is where the song similarity search should be connected
+            and progress bar updated
+        """
+        count = 0
+        self.progress.update()
+        self.progress["maximum"] = 100
+        time.sleep(1)
+
+        self.progress['value'] = 20
+        count = count + 1
+        self.l_songs_found["text"] = "Songs found...   " + str(count) + "/" + self.max_songs
+        self.progress.update()
+        time.sleep(1)
+
+        self.progress['value'] = 40
+        count = count + 1
+        self.l_songs_found["text"] = "Songs found...   " + str(count) + "/" + self.max_songs
+        self.progress.update()
+        time.sleep(1)
+
+        self.progress['value'] = 50
+        count = count + 1
+        self.l_songs_found["text"] = "Songs found...   " + str(count) + "/" + self.max_songs
+        self.progress.update()
+        time.sleep(1)
+
+        self.progress['value'] = 60
+        count = count + 1
+        self.l_songs_found["text"] = "Songs found...   " + str(count) + "/" + self.max_songs
+        self.progress.update()
+        time.sleep(1)
+
+        self.progress['value'] = 80
+        count = count + 1
+        self.l_songs_found["text"] = "Songs found...   " + str(count) + "/" + self.max_songs
+        self.progress.update()
+        time.sleep(1)
+
+        self.progress['value'] = 100
+        count = count + 1
+        self.l_songs_found["text"] = "Songs found...   " + str(count) + "/" + self.max_songs
+        self.progress.update()
+        time.sleep(1)
+
+
+
+    def close_window(self):
+        """override window closing event
+        """
+        self.progress.destroy()
+        self.win.destroy()
+        self.grab_release()
