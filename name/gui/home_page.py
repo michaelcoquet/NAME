caf4c06d11c80043e056a8094c38753ec7b02d5f
m <m.coquet2@gmail.com>
2020-11-16 03:02:30 -0600

    finishes the Member Home frame, the flow of the gui must be more closely controlled as in a finite state machine

--- name/gui/home_page.py
+++ name/gui/home_page.py
@@ -26,9 +26,13 @@ class HomePage(tk.Frame):
         self.progress = None
         self.l_songs_found = None
         self.signed_in = 0
+        self.init_home()
+        self.init_upper_grid()
+        self.init_lower_grid()
         self.init_member_menu()
         self.init_guest_menu()
-        self.init_widgets()
+        self.init_compare_songs_frame()
+        self.init_member_home_frame()
 
     def init_member_menu(self):
         """ make the menu for when a user links their spotify account
@@ -36,7 +40,7 @@ class HomePage(tk.Frame):
         self.member_menu = tk.Menu(self.master)
 
         self.my_account_menu = tk.Menu(self.member_menu, tearoff=0)
-        self.my_account_menu.add_command(label="Member Home")
+        self.my_account_menu.add_command(label="Member Home", command=self.member_home)
         self.my_account_menu.add_separator()
 
         # make a submenu for groups
@@ -59,7 +63,68 @@ class HomePage(tk.Frame):
         self.guest_menu.add_command(label="Login", command=self.login)
         self.master.config(menu=self.guest_menu)
 
-    def init_widgets(self):
+    def init_upper_grid(self):
+        """[summary]
+        """
+        self.upper_grid = tk.Frame(self.home_frame)
+        self.upper_grid.grid(row=0, column=0, sticky="ew")
+
+        # could put an image logo here if desired, for now just a label
+        self.app_title = tk.Label(self.upper_grid)
+        self.app_title["text"] = "N.A.M.E"
+        self.app_title.grid(row=0, column=1)
+
+        self.create_playlist_button = tk.Button(self.upper_grid)
+        self.create_playlist_button["text"] = "Create Playlist"
+        self.create_playlist_button.grid(row=1, column=0)
+        self.create_playlist_button["state"] = tk.DISABLED
+
+        self.compare_songs_button = tk.Button(self.upper_grid, command=self.compare_songs)
+        self.compare_songs_button["text"] = "Compare Songs"
+        self.compare_songs_button.grid(row=1, column=1)
+
+        self.get_song_info_button = tk.Button(self.upper_grid)
+        self.get_song_info_button["text"] = "Get Song Info"
+        self.get_song_info_button.grid(row=1, column=2)
+
+        # TODO: add the proper filters to the dropdown list
+        variable = StringVar(self.upper_grid)
+        variable.set("Filters") # default value
+        self.filters_dropdown = tk.OptionMenu(self.upper_grid, variable, "one", "two", "three",
+                                                    command=self.filter_function)
+        self.filters_dropdown.grid(row=2, column=0)
+
+        self.song_search_entry = tk.Entry(self.upper_grid)
+        self.song_search_entry.insert(0, "Song title")
+        self.song_search_entry.grid(row=2, column=1)
+
+        # TODO: connect with backend song search function
+        self.song_search_button = tk.Button(self.upper_grid)
+        self.song_search_button["text"] = "Search"
+        self.song_search_button.grid(row=2, column=2)
+
+        self.song_sim_score_label = tk.Label(self.upper_grid)
+
+    def init_lower_grid(self):
+        """[summary]
+        """
+        self.lower_grid = tk.Frame(self.home_frame)
+        Grid.columnconfigure(self.lower_grid, 1, weight=1)
+
+        self.lower_grid.grid(row=2, column=0, sticky="ew")
+
+        self.remove_all_button = tk.Button(self.lower_grid, command=self.rem_all_alert)
+        self.remove_all_button["text"] = "Remove all"
+        self.remove_all_button.grid(row=0, column=0)
+
+        self.start_over_button = tk.Button(self.lower_grid, command=self.start_over)
+        self.start_over_button["text"] = "Start Over"
+
+        self.create_similarity_playlist = tk.Button(self.lower_grid,
+            text="Find Similar Songs", command=self.open_sim_progress)
+        self.create_similarity_playlist.grid(row=0, column=2)
+
+    def init_home(self):
         """ create the home page widgets
         """
         #make home frame
@@ -67,71 +132,30 @@ class HomePage(tk.Frame):
         self.home_frame.grid(row=0, column=0, sticky="nsew")
         self.home_frame.tkraise()
 
-        upper_menu = tk.Frame(self.home_frame)
-        upper_menu.grid(row=0, column=0, sticky="ew")
-        lower_menu = tk.Frame(self.home_frame)
-        lower_menu.grid(row=2, column=0, sticky="ew")
-
         #set up grid
         Grid.columnconfigure(self.home_frame, 0, weight=1)
         Grid.rowconfigure(self.home_frame, 1, weight=1)
-        Grid.columnconfigure(lower_menu, 1, weight=1)
 
         self.grid(padx="10", pady="10")
-
-        # could put an image logo here if desired, for now just a label
-        self.app_title = tk.Label(upper_menu)
-        self.app_title["text"] = "N.A.M.E"
-        self.app_title.grid(row=0, column=1)
-
-        self.create_playlist = tk.Button(upper_menu)
-        self.create_playlist["text"] = "Create Playlist"
-        self.create_playlist.grid(row=1, column=0)
-        self.create_playlist["state"] = tk.DISABLED
-
-        self.compare_songs_button = tk.Button(upper_menu, command=self.compare_songs)
-        self.compare_songs_button["text"] = "Compare Songs"
-        self.compare_songs_button.grid(row=1, column=1)
-
-        self.get_song_info = tk.Button(upper_menu)
-        self.get_song_info["text"] = "Get Song Info"
-        self.get_song_info.grid(row=1, column=2)
-
-        self.song_sim_score_label = tk.Label(upper_menu)
-
-        # TODO: add the proper filters to the dropdown list
-        variable = StringVar(upper_menu)
-        variable.set("Filters") # default value
-        self.filters_dropdown = tk.OptionMenu(upper_menu, variable, "one", "two", "three",
-                                                    command=self.filter_function)
-        self.filters_dropdown.grid(row=2, column=0)
-
-        self.song_search = tk.Entry(upper_menu)
-        self.song_search.insert(0, "Song title")
-        self.song_search.grid(row=2, column=1)
-
-        # TODO: connect with backend song search function
-        self.song_search_button = tk.Button(upper_menu)
-        self.song_search_button["text"] = "Search"
-        self.song_search_button.grid(row=2, column=2)
 
         self.song_listbox = tk.Listbox(self.home_frame)
         self.song_listbox.grid(row=1, column=0, sticky="nsew")
 
-        self.remove_all_button = tk.Button(lower_menu, command=self.rem_all_alert)
-        self.remove_all_button["text"] = "Remove all"
-        self.remove_all_button.grid(row=0, column=0)
+    def init_member_home_frame(self):
+        self.latest_playlist_button = tk.Button(self.upper_grid)
+        self.latest_playlist_button["text"] = "Latest Playlist"
 
-        self.start_over_button = tk.Button(lower_menu, command=self.start_over)
-        self.start_over_button["text"] = "Start Over"
+        self.all_playlists_button = tk.Button(self.upper_grid)
+        self.all_playlists_button["text"] = "All Playlists"
 
-        self.get_stats_button = tk.Button(lower_menu, text="Get Stats", command=self.get_stats)
-        self.get_stats_button.grid(row=0, column=2)
+        self.listening_info_button = tk.Button(self.upper_grid)
+        self.listening_info_button["text"] = "Your Listening Info"
 
-        self.create_similarity_playlist = tk.Button(lower_menu,
-            text="Find Similar Songs", command=self.open_sim_progress)
-        self.create_similarity_playlist.grid(row=0, column=2)
+        self.playlist_listbox = tk.Listbox(self.home_frame)
 
+    def init_compare_songs_frame(self):
+        self.get_stats_button = tk.Button(self.lower_grid, command=self.get_stats)
+        self.get_stats_button["text"] = "Get Stats"
 
     def open_sim_progress(self):
         """open a new window that updates the user on the progress of similarity playlist
@@ -223,17 +247,17 @@ class HomePage(tk.Frame):
         """
         if frame_id == 0:
             # frame_id[0] = Compare Songs set up the frame for comparing two or more songs
-            self.song_search.delete(0, 20)
-            self.song_search.insert(0, "two or more songs")
+            self.song_search_entry.delete(0, 20)
+            self.song_search_entry.insert(0, "two or more songs")
             self.create_similarity_playlist.grid_forget()
             self.get_stats_button.grid(row=0, column=2)
             self.compare_songs_button["state"] = tk.DISABLED
-            self.create_playlist["state"] = tk.NORMAL
+            self.create_playlist_button["state"] = tk.NORMAL
             self.start_over_button.grid_forget()
             self.remove_all_button.grid(row=0, column=0)
             self.song_sim_score_label.grid_forget()
             self.filters_dropdown.grid(row=2, column=0)
-            self.song_search.grid(row=2, column=1)
+            self.song_search_entry.grid(row=2, column=1)
             self.song_search_button.grid(row=2, column=2)
         elif frame_id == 1:
             # frame_id[1] = get stats frame for displaying similarity comparison of two or more
@@ -243,15 +267,29 @@ class HomePage(tk.Frame):
             self.get_stats_button.grid_forget()
             self.filters_dropdown.grid_forget()
             self.song_search_button.grid_forget()
-            self.song_search.grid_forget()
+            self.song_search_entry.grid_forget()
             self.song_sim_score_label["text"] = "These songs are X% similar"
             self.song_sim_score_label.grid(row=2, column=0, columnspan=3)
         elif frame_id == 2:
-            # frame_id[2] = member frame, this is for when a guest has successfully linked their
-            #               spotify account
-
-            # many things change here, first change the dropdown menu
+            # frame_id[2] = login, this is for when a guest has successfully linked
+            #               their spotify account
             self.master.config(menu=self.member_menu)
+        elif frame_id == 3:
+            # frame_id[3] = home page, home page for guest/member to search for similar songs
+            print("TODO")
+        elif frame_id == 4:
+            # frame_id[4] = member home page, home page for member
+            self.create_playlist_button.grid_forget()
+            self.compare_songs_button.grid_forget()
+            self.get_song_info_button.grid_forget()
+            self.filters_dropdown.grid_forget()
+            self.song_search_button.grid_forget()
+            self.song_search_entry.grid_forget()
+            self.song_listbox.grid_forget()
+            self.latest_playlist_button.grid(row=0, column=0)
+            self.all_playlists_button.grid(row=0, column=1)
+            self.listening_info_button.grid(row=0, column=2)
+            self.playlist_listbox.grid(row=1, column=0, sticky="nsew")
         else:
             print("error")
 
@@ -291,6 +329,12 @@ class HomePage(tk.Frame):
                   storyboards
         """
         self.group_menu.add_command(label="new group name") # TODO: make a real test and connect
+
+    def member_home(self):
+        """ Home page for a member that has a spotify account linked
+            TODO: backend connection required to load members playlists
+        """
+        self.switch_frame(4)
 
     @staticmethod #remove later
     def rem_all_alert():
