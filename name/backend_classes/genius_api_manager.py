7fd4b5435a21a765fa0b1ac18a0fb53a4e7c8211
smw279 <64109896+smw279@users.noreply.github.com>
2020-11-20 12:58:02 -0600

    Added a search function to the genius api which returns a json

--- name/backend_classes/genius_api_manager.py
+++ name/backend_classes/genius_api_manager.py
@@ -1,14 +1,21 @@
+import lyricsgenius
 from lyricsgenius import genius
 
-class my_class(object):
+class genius_api_manager(object):
 
     def __init__(self):
         """Account: The user's Spotify account"""
-        genius = lyricsgenius.Genius("F91niKdwkbdKfbtYZzg1IYm4xoze0Y2Pwu1P0q2WgU68ZydNM0FdrpXQqXxD_-Td")
+        self.my_genius = lyricsgenius.Genius("F91niKdwkbdKfbtYZzg1IYm4xoze0Y2Pwu1P0q2WgU68ZydNM0FdrpXQqXxD_-Td")
 
-    def search_for_lyrics():
+    def search_for_lyrics(self, song_name, song_artist):
+        """TODO: this docstring"""
+        song = self.my_genius.search_song(song_name, song_artist)
+        song_json = song.to_json
         return ""
     
+
+test = genius_api_manager()
+test.search_for_lyrics("country roads", "John Denver")
 
 
 
