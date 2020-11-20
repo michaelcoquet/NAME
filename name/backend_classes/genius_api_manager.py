eb6a1b3e9a0791177c2e234c8098cffbb7774158
smw279 <64109896+smw279@users.noreply.github.com>
2020-11-20 13:43:04 -0600

    Added documentation to the genius api manager class. Have some basic temporary tests at the bottom of the file which will be made into proper tests later.

--- name/backend_classes/genius_api_manager.py
+++ name/backend_classes/genius_api_manager.py
@@ -4,17 +4,33 @@ from lyricsgenius import genius
 class genius_api_manager(object):
 
     def __init__(self):
-        """Account: The user's Spotify account"""
-        self.my_genius = lyricsgenius.Genius("F91niKdwkbdKfbtYZzg1IYm4xoze0Y2Pwu1P0q2WgU68ZydNM0FdrpXQqXxD_-Td")
+        """Initialization function to authenticate the program's
+        connection to the genius API
+        """
+        self.my_genius = lyricsgenius.Genius("F91niKdwkbdKfbtYZzg1IYm4xoze0Y2Pwu1P0q2WgU68ZydN"
+                                              + "M0FdrpXQqXxD_-Td")
 
     def search_for_lyrics(self, song_name, song_artist):
-        """TODO: this docstring"""
-        song_lyrics = self.my_genius.search_song(song_name, song_artist).lyrics
+        """Searches the Genius API and returns the song's lyrics
+        returns None if no song or lyrics found.
+        song_name: the name of the desired song
+        song_artist: the desired song's artist
+        """
+        try:
+            song_lyrics = self.my_genius.search_song(song_name, song_artist).lyrics
+        except:
+            #return None if no song or lyrics found
+            return None
         return song_lyrics
     
 
 test = genius_api_manager()
-print(test.search_for_lyrics("country roads", "John Denver"))
+#print(test.search_for_lyrics("country roads", "John Denver"))
+#print(test.search_for_lyrics("country roads", "Kanye"))
+#print(test.search_for_lyrics("country roads", None))
+#print(test.search_for_lyrics("7428394782309470892", "John Denver"))
+#test.search_for_lyrics("7428394782309470892", "John Denver")
+
 
 
 
