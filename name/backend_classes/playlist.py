2018160b39a98f7a036ad8900d52a33cb94d25ff
Laurence Garcia <lsg913@usask.ca>
2020-11-21 19:05:43 -0600

    Updated the playlist class * remove_song function is now functional * updated the testing part: changed the size of the test playlist into a smaller scale, for faster testing. * added a test for the functions: add_song and remove_song

--- name/backend_classes/playlist.py
+++ name/backend_classes/playlist.py
@@ -22,13 +22,26 @@ class Playlist:
         """
         A function that adds song to the playlist,
         appends a song to the list of songs
+        song: a song object to be added to the list
         """
         self.songs.append(song)
         self.size = self.size + 1
 
-    # TODO:
     def remove_song(self, song_id):
-        pass
+        """
+        A function that removes a song from the playlist
+        base on its index
+        song_id: the id of the song to be removed from the playlist
+        """
+        # A temporary list for the song ids
+        temp = []
+        for i in self.songs:
+            temp.append(i.song_id)
+
+        if song_id in temp:
+            song_idx = temp.index(song_id)
+            self.songs.pop(song_idx)
+            self.size = self.size - 1
 
     def __str__(self):
         return ("Playlist Name: {} \n"
@@ -40,41 +53,59 @@ class Playlist:
                                     [i.song_name for i in self.songs])
 
 
-# # Testing
+# Testing
+# Just some random playlist
+test_playlist = '3xSYFlYjXkR8cK2plz82R0'
+client_id = '0e48c2ec84d3401e9262a2159a277d82'
+client_secret = 'aa650130a5b544598f4b058bfd264b21'
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(auth_manager=auth_manager)
 
-# test_playlist = '37i9dQZF1DWSWllk9RmgZM'
-# client_id = '0e48c2ec84d3401e9262a2159a277d82'
-# client_secret = 'aa650130a5b544598f4b058bfd264b21'
-# auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-# spotify = spotipy.Spotify(auth_manager=auth_manager)
+# Get playlist and song from spotify
+playlist1 = spotify.playlist(test_playlist)
+song1 = spotify.track('4oVdhvxZrKQTM9ZsUIZa3S')
+a_song = S.Song(song1)
+# print(a_song)
+playlist_test = Playlist(playlist1)
+# May take a few seconds depending on the playlist size
+print(playlist_test)
 
-# # Get playlist from spotify
-# playlist1 = spotify.playlist(test_playlist)
+# Test for the playlist name
+expected = 'neck deep acoustic'
+test1 = playlist_test.playlist_name
+if expected != test1:
+    print("Test1: Error, did not meet the expected result")
 
-# playlist_test = Playlist(playlist1)
-# # May take a few seconds depending on the playlist size
-# print(playlist_test)
+# Test for the playlist owner
+expected2 = 'annie'
+test2 = playlist_test.playlist_owner
+if expected2 != test2:
+    print("Test2: Error, did not meet the expected result")
 
-# # Test for the playlist name
-# expected = 'This Is Post Malone'
-# test1 = playlist_test.playlist_name
-# if expected != test1:
-#     print("Test1: Error, did not meet the expected result")
+# Test for the playlist id
+expected3 = '3xSYFlYjXkR8cK2plz82R0'
+test3 = playlist_test.playlist_id
+if expected3 != test3:
+    print("Test3: Error, did not meet the expected result")
 
-# # Test for the playlist owner
-# expected2 = 'Spotify'
-# test2 = playlist_test.playlist_owner
-# if expected2 != test2:
-#     print("Test2: Error, did not meet the expected result")
+# Test for the playlist size
+expected4 = 11
+test4 = playlist_test.size
+if expected4 != test4:
+    print("Test4: Error, did not meet the expected result")
 
-# # Test for the playlist id
-# expected3 = '37i9dQZF1DWSWllk9RmgZM'
-# test3 = playlist_test.playlist_id
-# if expected3 != test3:
-#     print("Test3: Error, did not meet the expected result")
+# Function tests
 
-# # Test for the playlist size
-# expected4 = 45
-# test4 = playlist_test.size
-# if expected4 != test4:
-#     print("Test4: Error, did not meet the expected result")
+# Test for adding a song to the playlist
+expected5 = a_song
+playlist_test.add_song(a_song)
+test5 = playlist_test.songs
+if expected5 not in test5:
+    print("Test5: Error, did not meet the expected result")
+
+# Test for removing a song from the playlist
+expected6 = a_song.song_name
+playlist_test.remove_song(a_song.song_id)
+test6 = playlist_test.songs
+if expected6 in test6:
+    print("Test6: Error, did not meet the expected result")
