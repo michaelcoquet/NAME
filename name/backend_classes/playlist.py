4b353baf303e4c7578103d578de3831e04f811a4
Laurence Garcia <lsg913@usask.ca>
2020-11-22 16:35:22 -0600

    * Updated the song, playlist, and some parts of the spotify api manager class  - song class is no longer dependant on the spotify api manager class  - made some changes on the spotify api manager functions: search_songs and get_audio_features     - search_songs: changed the way song class is creating song objects     - get_audio_features: changed the parameter into a single song_id instead of list of songs objects

--- name/backend_classes/playlist.py
+++ name/backend_classes/playlist.py
@@ -1,4 +1,3 @@
-import song as S
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -7,16 +6,17 @@ class Playlist:
     """
     A class containing the details of the playlist from spotify
     """
-    def __init__(self, playlist):
+    def __init__(self, playlist, playlist_tracks):
         """
         Instantiation function
         playlist: a playlist object from spotify
+        playlist_tracks: a list of song objects containing the playlist tracks
         """
         self.playlist_name = playlist['name']
         self.playlist_owner = playlist['owner']['display_name']
         self.playlist_id = playlist['id']
         self.size = playlist['tracks']['total']
-        self.songs = [S.Song(i['track']) for i in playlist['tracks']['items']] # Store each track of the playlist into a list of songs
+        self.songs = playlist_tracks
 
     def add_song(self, song):
         """
@@ -39,8 +39,8 @@ class Playlist:
             temp.append(i.song_id)
 
         if song_id in temp:
-            song_idx = temp.index(song_id)
-            self.songs.pop(song_idx)
+            song_index = temp.index(song_id)
+            self.songs.pop(song_index)
             self.size = self.size - 1
 
     def __str__(self):
@@ -53,59 +53,59 @@ class Playlist:
                                     [i.song_name for i in self.songs])
 
 
-# Testing
-# Just some random playlist
-test_playlist = '3xSYFlYjXkR8cK2plz82R0'
-client_id = '0e48c2ec84d3401e9262a2159a277d82'
-client_secret = 'aa650130a5b544598f4b058bfd264b21'
-auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-spotify = spotipy.Spotify(auth_manager=auth_manager)
+# # Testing
+# # Just some random playlist
+# test_playlist = '3xSYFlYjXkR8cK2plz82R0'
+# client_id = '0e48c2ec84d3401e9262a2159a277d82'
+# client_secret = 'aa650130a5b544598f4b058bfd264b21'
+# auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+# spotify = spotipy.Spotify(auth_manager=auth_manager)
 
-# Get playlist and song from spotify
-playlist1 = spotify.playlist(test_playlist)
-song1 = spotify.track('4oVdhvxZrKQTM9ZsUIZa3S')
-a_song = S.Song(song1)
-# print(a_song)
-playlist_test = Playlist(playlist1)
-# May take a few seconds depending on the playlist size
-print(playlist_test)
+# # Get playlist and song from spotify
+# playlist1 = spotify.playlist(test_playlist)
+# song1 = spotify.track('4oVdhvxZrKQTM9ZsUIZa3S')
+# a_song = S.Song(song1)
+# # print(a_song)
+# playlist_test = Playlist(playlist1)
+# # May take a few seconds depending on the playlist size
+# print(playlist_test)
 
-# Test for the playlist name
-expected = 'neck deep acoustic'
-test1 = playlist_test.playlist_name
-if expected != test1:
-    print("Test1: Error, did not meet the expected result")
+# # Test for the playlist name
+# expected = 'neck deep acoustic'
+# test1 = playlist_test.playlist_name
+# if expected != test1:
+#     print("Test1: Error, did not meet the expected result")
 
-# Test for the playlist owner
-expected2 = 'annie'
-test2 = playlist_test.playlist_owner
-if expected2 != test2:
-    print("Test2: Error, did not meet the expected result")
+# # Test for the playlist owner
+# expected2 = 'annie'
+# test2 = playlist_test.playlist_owner
+# if expected2 != test2:
+#     print("Test2: Error, did not meet the expected result")
 
-# Test for the playlist id
-expected3 = '3xSYFlYjXkR8cK2plz82R0'
-test3 = playlist_test.playlist_id
-if expected3 != test3:
-    print("Test3: Error, did not meet the expected result")
+# # Test for the playlist id
+# expected3 = '3xSYFlYjXkR8cK2plz82R0'
+# test3 = playlist_test.playlist_id
+# if expected3 != test3:
+#     print("Test3: Error, did not meet the expected result")
 
-# Test for the playlist size
-expected4 = 11
-test4 = playlist_test.size
-if expected4 != test4:
-    print("Test4: Error, did not meet the expected result")
+# # Test for the playlist size
+# expected4 = 11
+# test4 = playlist_test.size
+# if expected4 != test4:
+#     print("Test4: Error, did not meet the expected result")
 
-# Function tests
+# # Function tests
 
-# Test for adding a song to the playlist
-expected5 = a_song
-playlist_test.add_song(a_song)
-test5 = playlist_test.songs
-if expected5 not in test5:
-    print("Test5: Error, did not meet the expected result")
+# # Test for adding a song to the playlist
+# expected5 = a_song
+# playlist_test.add_song(a_song)
+# test5 = playlist_test.songs
+# if expected5 not in test5:
+#     print("Test5: Error, did not meet the expected result")
 
-# Test for removing a song from the playlist
-expected6 = a_song.song_name
-playlist_test.remove_song(a_song.song_id)
-test6 = playlist_test.songs
-if expected6 in test6:
-    print("Test6: Error, did not meet the expected result")
+# # Test for removing a song from the playlist
+# expected6 = a_song.song_name
+# playlist_test.remove_song(a_song.song_id)
+# test6 = playlist_test.songs
+# if expected6 in test6:
+#     print("Test6: Error, did not meet the expected result")
--- name/backend_classes/song.py
+++ name/backend_classes/song.py
@@ -6,7 +6,7 @@ class Song:
     A class for Song that stores the collected data
     from Spotify.
     """
-    def __init__(self, song):
+    def __init__(self, song, song_details):
         """
         song: A song object containing the data of the song from Spotify
         """
@@ -14,16 +14,16 @@ class Song:
         self.song_id = song['id']
         self.song_artist = Artist(song['artists'])
         self.album_details = Album(song['album'])
-        self.features = SongDetails(self.get_audio_features())
+        self.audio_features = SongDetails(song_details)
 
     # Placeholder function until spotify api manager implements it
-    def get_audio_features(self):
-        client_id='805b88ab14814deea1ae9a0f650b12fc'
-        client_secret='c20fae8240cb44e0aa720ed23053f307'
-        auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-        sp = spotipy.Spotify(auth_manager=auth_manager)
-        features = sp.audio_features(self.song_id)
-        return features[0]
+    # def get_audio_features(self):
+    #     client_id='805b88ab14814deea1ae9a0f650b12fc'
+    #     client_secret='c20fae8240cb44e0aa720ed23053f307'
+    #     auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+    #     sp = spotipy.Spotify(auth_manager=auth_manager)
+    #     features = sp.audio_features(self.song_id)
+    #     return features[0]
 
     # def __eq__(self, other):
     #     """
@@ -41,7 +41,7 @@ class Song:
         return ("Name: {} \n"
                 "Artist: {} \n"
                 "--Details--{}").format(self.song_name, self.song_artist.name,
-                                        self.features)
+                                        self.audio_features)
 
     def __repr__(self):
         return self.song_name
@@ -66,7 +66,7 @@ class Artist:
         return False
 
     def __str__(self):
-        return f"{self.artist_id} {self.name}"
+        return f"{self.artist_id,self.name}"
 
 
 class Album:
@@ -118,21 +118,20 @@ class SongDetails:
         self.valence = details['valence']
         self.time_signature = details['time_signature']
 
-
     def __str__(self):
-        return ("\n-Duration: {} \n"
-                "-Key: {} \n"
-                "-Tempo: {} \n"
-                "-Danceability: {} \n"
-                "-Energy: {} \n"
-                "-Loudness: {} \n"
-                "-Mode: {} \n"
-                "-Speechiness: {} \n"
-                "-Acousticness: {} \n"
-                "-Instrumentalness: {} \n"
-                "-Liveness: {} \n"
-                "-Valence: {} \n"
-                "-Time Signature: {}").format(self.duration, self.key,
+        return ("\nDuration: {} \n"
+                "Key: {} \n"
+                "Tempo: {} \n"
+                "Danceability: {} \n"
+                "Energy: {} \n"
+                "Loudness: {} \n"
+                "Mode: {} \n"
+                "Speechiness: {} \n"
+                "Acousticness: {} \n"
+                "Instrumentalness: {} \n"
+                "Liveness: {} \n"
+                "Valence: {} \n"
+                "Time Signature: {}").format(self.duration, self.key,
                                               self.tempo, self.danceability,
                                               self.energy, self.loudness,
                                               self.mode, self.speechiness,
@@ -141,5 +140,31 @@ class SongDetails:
                                               self.time_signature)
 
 
-# Test for Song Class
+# Testing
 
+# client_id = '0e48c2ec84d3401e9262a2159a277d82'
+# client_secret = 'aa650130a5b544598f4b058bfd264b21'
+# auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+# spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+# # a random song
+# song1 = '4oVdhvxZrKQTM9ZsUIZa3S'
+# test_song = spotify.track(song1)
+# a_song = Song(test_song)
+
+# # Test for song name
+# expected = 'December'
+# test1 = a_song.song_name
+# if expected != test1:
+#     print("Test1: Error, did not meet the expected result")
+
+# # Test for song id
+# expected2 = '4oVdhvxZrKQTM9ZsUIZa3S'
+# test2 = a_song.song_id
+# if expected2 != test2:
+#     print("Test2: Error, did not meet the expected result")
+
+# # Test for song artists
+# test3 = a_song.song_artist
+
+# # Test for song
