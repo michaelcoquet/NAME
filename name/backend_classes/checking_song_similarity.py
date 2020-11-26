e9ff184dcd9230018b696a9dceb34fa0cde7da75
benja <bec284@usask.ca>
2020-11-26 16:49:02 -0600

    Change to checking_song_similarity

--- name/backend_classes/checking_song_similarity.py
+++ name/backend_classes/checking_song_similarity.py
@@ -9,6 +9,11 @@ class CheckingSongSimilarity:
         self.features = features
 
     def random_search(self, original_song):
+        """ A function to search for 25 similar songs
+        to a song the user searches for
+        original_song: the song the user is searching for
+        returns: a list of 25 songs
+        """
         new_query = Query(self.features)
         similar_songs = []
         while len(similar_songs) < 26:
@@ -17,10 +22,12 @@ class CheckingSongSimilarity:
             random_string += "%"
             found_songs = new_query.search_single_song(random_string)
             for song in found_songs:
-                song_similarity = SongSimilarity([song, original_song], self.features)
+                original_song.append(song)
+                song_similarity = SongSimilarity(original_song, self.features)
                 similarity = song_similarity.compare_all()
                 if similarity >= 0.8:
                     similar_songs.append(song)
+                original_song.remove(song)
             
         return similar_songs
 
