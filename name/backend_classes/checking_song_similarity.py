21e3cece42d4074868f04d755aac3665d5b16a0c
benja <bec284@usask.ca>
2020-11-26 12:18:18 -0600

    Fixed issues with CheckingSongSimilarity

--- name/backend_classes/checking_song_similarity.py
+++ name/backend_classes/checking_song_similarity.py
@@ -5,12 +5,11 @@ from name.backend_classes.song_similarity import SongSimilarity
 
 class CheckingSongSimilarity:
 
-    def __init__(self, songs, features):
-        self.songs = songs
+    def __init__(self, features):
         self.features = features
 
-    def random_search(self, original_song, filter_list):
-        new_query = Query(filter_list)
+    def random_search(self, original_song):
+        new_query = Query(self.features)
         similar_songs = []
         while len(similar_songs) < 26:
             lowercase_letters = string.ascii_lowercase
@@ -18,14 +17,14 @@ class CheckingSongSimilarity:
             random_string += "%"
             found_songs = new_query.search_single_song(random_string)
             for song in found_songs:
-                song_similarity = SongSimilarity([song, original_song], filter_list)
+                song_similarity = SongSimilarity([song, original_song], self.features)
                 similarity = song_similarity.compare_all()
                 if similarity >= 0.8:
                     similar_songs.append(song)
             
         return similar_songs
 
-    def get_songs_similarity_score(self, songs, filter_list):
-        song_similarity = SongSimilarity(songs, filter_list)
+    def get_songs_similarity_score(self, songs):
+        song_similarity = SongSimilarity(songs, self.features)
         similarity = song_similarity.compare_all()
         return similarity * 100
