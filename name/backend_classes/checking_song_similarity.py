c19530ff9615bb149f23813414b0858641ef08d0
Liz Reid <emr237@mail.usask.ca>
2020-11-27 01:52:00 -0600

    Made some small changes so that offsets could be used in the random search function. Tried to improve the random search function so it is faster, but it still needs work since it has issues if the list of given songs aren't already at least as similar as the random search threshold

--- name/backend_classes/checking_song_similarity.py
+++ name/backend_classes/checking_song_similarity.py
@@ -17,17 +17,36 @@ class CheckingSongSimilarity:
         new_query = Query(self.features)
         similar_songs = []
         while len(similar_songs) < 26:
+            # get a random string. % is a wildcard character
             lowercase_letters = string.ascii_lowercase
-            random_string = "".join(random.choice(lowercase_letters) for i in range(5))
-            random_string += "%"
-            found_songs = new_query.search_single_song(random_string)
-            songs.append(found_songs[0])
-            song_similarity = SongSimilarity(songs, self.features)
-            similarity = song_similarity.compare_all()
-            if similarity >= 0.8:
-                similar_songs.append(found_songs[0])
-            songs.remove(found_songs[0])
-            
+            random_letter1 = random.choice(lowercase_letters)
+            random_string = random_letter1 + "%"
+            # max offset for spotify is 2000, but keeping this
+            # at 100 is faster
+            random_offset = random.randint(0, 2000)
+            # in some cases the random offset may be too large,
+            # and in that case default to 0
+            try: 
+                found_songs = new_query.search_single_song(random_string, offset=random_offset)
+            except: 
+                found_songs = new_query.search_single_song(random_string)
+            # Look at all the found songs and see if any are similar enough
+            # to be included
+            for song in found_songs:
+                # current song names
+                similar_song_names = [sim_song.song_name for sim_song in similar_songs]
+                if song.song_name not in similar_song_names:
+                    songs.append(song)
+                    song_similarity = SongSimilarity(songs, self.features)
+                    similarity = song_similarity.compare_all()
+                    if similarity >= 0.80:
+                        similar_songs.append(song)
+                        # run a new search after this
+                        songs.remove(song)
+                        break
+                    else:
+                        songs.remove(song)
+        print(similar_songs)    
         return similar_songs
 
     def get_songs_similarity_score(self, songs):
