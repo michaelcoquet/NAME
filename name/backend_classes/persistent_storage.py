be012ef4d9b7b877104fc48cbf1630a3051f298e
m <m.coquet2@gmail.com>
2020-11-30 11:44:08 -0600

    adds a new persistent_storage class and adds stubs for the classes functions

---
+++ name/backend_classes/persistent_storage.py
@@ -0,0 +1,92 @@
+"""CMPT 370 Group 5 Project: NAME (Nearly Analogous Music Engine)
+    Summary:  is a piece of software to compare songs using the Spotify API. NAME will help the
+              user find other similar songs to the ones they are interested in, as well as
+              detailed info about their favorite songs.
+"""
+import json
+
+class persistent_storage:
+    """ class to handle the storage of data in json format (see wiki persistent storage)
+    """
+
+    def __init__(self, unencrypted_spotify_id):
+        """ unencrypted_spotify_id : the users unencrypted spotify ID, this class will encrypt
+                                     the id before saving to disk for privacy concerns
+        """
+        self.unencrypted_spotify_id = unencrypted_spotify_id
+
+    def create_new_member(self):
+        """ This will initiate a new user in the database, this will simply create a blank entry
+            with a corresponding spotify id and will need to be loaded with users data in
+            another step
+        """
+        return 1
+
+    def check_if_user_exists(self):
+        """ check if the user already has information stored
+        """
+        return 1
+
+    def check_if_playlist_exists(self, playlist_id):
+        """ check if a the given playlist id exists for a user in either spotify or json
+
+        Args:
+            playlist_id(string): the spotify id for the desired playlist
+        """
+        return 1
+
+    def check_if_group_exists(self, group_id):
+        """ check if the given group exists or not
+
+        Args:
+            group_id (integer): the id for the desired group
+        """
+        return 1
+
+    def save_all_existing_playlists(self):
+        """ find all the playlists for the user and save them to our json file
+        """
+        return 1
+
+    def save_new_playlist(self, new_playlist):
+        """ save a new playlist to the users file
+
+        Args:
+            new_playlist(Playlist): the new playlist to be saved to the users file
+        """
+        return 1
+
+    def update_playlist(self, playlist_id):
+        """ update a playlist in the users file
+
+        Args:
+            playlist_id(string): the spotify id for the desired playlist
+        """
+        return 1
+
+    def save_new_group(self, new_group):
+        """ save a new group to the users file
+
+        Args:
+            new_group(Group): the new group to be save to the users file
+        """
+        return 1
+
+    def update_group(self, group_id):
+        """ update the given group
+
+        Args:
+            group_id (integer): the groups id to be updated
+        """
+        return 1
+
+    def encrypt(self, input):
+        """ function to help encrypt the data that needs encrypting
+
+        Args:
+            input (string): the input string to be encrypted
+
+        returns:
+            output (string): the enrypted input string
+        """
+        return 1
