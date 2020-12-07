8b403600a26ea431eedc11a9d2fef8c6fa3af6d9
m <m.coquet2@gmail.com>
2020-12-07 15:59:39 -0600

    fixes a bug that would show the results of a search twice in the treeview

--- name/gui/home_page_frame.py
+++ name/gui/home_page_frame.py
@@ -263,9 +263,8 @@ class HomePageFrame(NameFrame):
         self.song_treeview.delete(*self.song_treeview.get_children())
         self.parent.song_object_list.clear()
         self.parent.song_object_list = results
-        self.parent.frames[self.parent.get_frame_id("Search Results")].display_data(results)
         # switch to search results frame, and give it the results to be displayed
-        self.switch_frame("Search Results")
+        self.parent.switch_to_previous_frame()
 
         # enable the button again
         self.similar_songs_button.configure(state="normal")
