e010632549548128810f92498391039e201294af
mike <m.coquet2@gmail.com>
2021-06-23 14:47:39 -0600

    switches to using postgresql and dockerizes the app

--- NAME/settings.py
+++ NAME/settings.py
@@ -77,15 +77,16 @@ WSGI_APPLICATION = "NAME.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+        "ENGINE": os.getenv("SQL_ENGINE"),
+        "NAME": os.getenv("APP_DB_NAME"),
+        "USER": os.getenv("APP_DB_USER"),
+        "PASSWORD": os.getenv("APP_DB_PASS"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
