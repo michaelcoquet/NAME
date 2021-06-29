31b9e05e9648e275a7331b2c49a639a6cbb088f5
mike <m.coquet2@gmail.com>
2021-06-29 12:36:51 -0600

    adds back in the check for debugging state

--- manage.py
+++ manage.py
@@ -10,13 +10,13 @@ def main():
     # start new section
     from django.conf import settings
 
-    if os.environ.get("RUN_MAIN") or os.environ.get("WERKZEUG_RUN_MAIN"):
-        import ptvsd
+    if settings.DEBUG:
+        if os.environ.get("RUN_MAIN") or os.environ.get("WERKZEUG_RUN_MAIN"):
+            import ptvsd
 
-        ptvsd.enable_attach(address=("0.0.0.0", 3000))
-        ptvsd.wait_for_attach()
-        ptvsd.break_into_debugger()
-    # end new section
+            ptvsd.enable_attach(address=("0.0.0.0", 3000))
+            ptvsd.wait_for_attach()
+            ptvsd.break_into_debugger()
 
     try:
         from django.core.management import execute_from_command_line
