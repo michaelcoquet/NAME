a0377ffd44e133a49cbd62a5f64b95753a9dcbbe
mike <m.coquet2@gmail.com>
2021-07-08 22:04:00 -0600

    finishes building users playlists, still need models for tracks to be associated with a specific users account

--- account/social_pipeline.py
+++ account/social_pipeline.py
@@ -2,8 +2,8 @@ from account.models import Profile
 
 
 def connect_spotify(backend, user, response, *args, **kwargs):
-    if backend.name == "spotify":
-        profile = Profile.objects.filter(user=user)
+    profile = Profile.objects.filter(user=user)
+    if backend.name == "spotify" and profile.count() == 1:
         profile = profile.get()
         profile.spotify_connected = True
         profile.save()
