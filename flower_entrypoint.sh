d8577a8ae1d4a21ec04ce66b1bbfee51abf10733
mike <m.coquet2@gmail.com>
2021-07-20 00:22:58 -0600

    fixes the flower bug

--- flower_entrypoint.sh
+++ flower_entrypoint.sh
@@ -1,10 +1,8 @@
-# #!/bin/sh
+#!/bin/sh
 
-# until python3 manage.py celery -A project inspect ping; do
-#     >&2 echo "Celery workers not available"
-# done
+until celery -A NAME inspect ping; do
+    >&2 echo "Celery workers not available"
+done
 
-# echo 'Starting flower'
-# python3 manage.py celery -A project flower
-
-# exec "$@"
+echo 'Starting flower'
+celery -A NAME flower
