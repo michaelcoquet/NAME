7263db7bc5de42f272e4d7cf1e634272b88c3494
mike <mail@michaelcoquet.ca>
2021-08-17 21:21:32 -0600

    WIP switches everything over to using SQS and RDS for the message queue and database, wasnt as difficult to switch as expected actually quite painless, except the analyze_profile function message is too long for SQS so instead of sending an entire json representation of the users profile just send the users id and do the query on the other side

--- NAME/settings/dev.py
+++ NAME/settings/dev.py
@@ -4,10 +4,13 @@ DEBUG = True
 DATABASES = {
     "default": {
         "ENGINE": os.getenv("SQL_ENGINE"),
-        "NAME": os.getenv("APP_DB_NAME"),
-        "USER": os.getenv("APP_DB_USER"),
-        "PASSWORD": os.getenv("APP_DB_PASS"),
+        "NAME": os.getenv("POSTGRES_NAME"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST"),
         "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
+
+BROKER_URL = os.getenv("CELERY_BROKER_URL")
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
