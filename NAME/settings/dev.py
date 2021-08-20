4544de8c329b1e28caca528a1de40a77f0f55383
mike <mail@michaelcoquet.ca>
2021-08-20 00:06:45 -0600

    finally fixes the 403 bug that stops users who created their spotify account through facebook, only thing I changed was using a different account for the spotify api, also finishes a solid production environment for launching to aws, would like to set up an automated method to easily launch to aws in the future possibly with makefiles

--- NAME/settings/dev.py
+++ NAME/settings/dev.py
@@ -1,6 +1,7 @@
 from .base import *
 
 DEBUG = True
+
 DATABASES = {
     "default": {
         "ENGINE": os.getenv("SQL_ENGINE"),
--- nginx_entrypoint.prod.sh
+++ nginx_entrypoint.prod.sh
@@ -2,7 +2,7 @@
 
 echo "starting nginx"
 mkdir -p ./etc/nginx/ssl
-cp ./config/default-nginx.conf ./etc/nginx/nginx.conf
+cp ./config/default-nginx.conf ./etc/nginx/nginx.prod.conf
 cp ssl/name.crt ./etc/nginx/ssl/name.crt
 cp ssl/name.key ./etc/nginx/ssl/name.key
 chmod 775 -R ./static 
